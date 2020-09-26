a894dd2aa82d5439e8023624aa61914601402d7c
Dan Watson <dan.watson2309@gmail.com>
2020-09-26 22:03:55 +0100

    basic html complete and started JS

---
+++ main.js
@@ -0,0 +1,13 @@
+function calculateSplit(){
+    let total = document.querySelector(billTotal).nodeValue;
+    let service = document.querySelector(servQual).nodeValue
+    let noPeople = document.querySelector(people).nodeValue
+
+    if(calculateSplit){
+        total*service/noPeople
+        return
+
+    }
+    
+    
+}
