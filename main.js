53525143e82d2d5c9bebad8067f1c5a1373e3ad5
Dan Watson <dan.watson2309@gmail.com>
2020-10-02 20:41:00 +0100

    restart

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-let totalCost = document.querySelector("#billTotal");
-let noPeople = document.querySelector("#people");
-let button = document.querySelector("#calcBill");
-let eachPay = eachTotal(totalCost, noPeople);
 
-button = addEventListener("click", giveResult)
-
-
-function eachTotal(totalCost, noPeople){
-    return totalCost/noPeople
-}
-
- function giveResult(eachPay){
-    document.querySelector("#eachTotal").innerText = eachPay
- }
-
-
-    
