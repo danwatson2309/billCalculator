779a42b901133705c1ea1682d1c0fbff72c307eb
Dan <Dan@Dans-MacBook-Air.local>
2020-10-10 17:03:51 +0100

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+let billTotal = document.querySelector("#bill-total").innerHTML
+let noPeople = document.querySelector("#no-people")
+let tipPerc = document.querySelector("#tip-percent")
+let tipTotalSum = document.querySelector("#tip-total")
+let grandTotal = document.querySelector("#grand-total")
+let eachOwes = document.querySelector("#each-owes")
+
+function calcTip(){
+    let tipTotal = tipPerc/100 * billTotal
+    tipTotalSum.innerText = (`${tipTotal}`)
+    console.log(tipTotal)
+
+}
+
+/*function calculateBill(){
+grandTotal = billTotal + tipTotal
+
+}*/
+
+
+
+
+let button = document.querySelector("#calculate").addEventListener("click", calcTip);
