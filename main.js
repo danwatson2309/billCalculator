b289fe2a6c5935f6b1bfc16fb27c45e0a9d9e743
Dan Watson <dan.watson2309@gmail.com>
2020-10-15 21:50:39 +0100

    still not working

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
+let billTotal = 350 //document.querySelector("#bill-total").innerHTML;
+let noPeople = 3 //document.querySelector("#no-people").innerHTML;
+let tipPerc = 10 //document.querySelector("#tip-percent").innerHTML;
 
-const billTotal = document.querySelector("#bill-total").innerHTML;
-const noPeople = document.querySelector("#no-people").innerHTML;
-const tipPerc = document.querySelector("#tip-percent").innerHTML;
-const tipTotal = tipPerc/100 * billTotal;
-const grandTotal = billTotal + tipTotal;
-const eachowes = grandTotal / noPeople;
 
- function calcTip(tipPerc, billTotal){
-     let tipTotal = tipPerc/100 * billTotal;
-    console.log(tipTotal)
-}
+let tipTotal = calcTip(tipPerc, billTotal) 
+
+function calcTip(tipPerc, billTotal){
+    return tipPerc/100 * billTotal;
+};
+
+let grandTotal = totalCost(billTotal, tipTotal);
+
+function totalCost(billTotal, tipTotal){
+    return billTotal + tipTotal
+};
+
+let eachowes = totalEach(grandTotal, noPeople);
+let eachowess = eachowes.toFixed(2);
+
+function totalEach(grandTotal, noPeople){
+    return grandTotal / noPeople
+};
 
 let button = document.querySelector("#calculate").addEventListener("click", calcTip)
-document.querySelector("#each-owes").innerText = `£${eachowes}`;
+document.querySelector("#each-owes").innerText = `£${eachowess}`;
 document.querySelector("#grand-total").innerText = `£${grandTotal}`;
 document.querySelector("#tip-total").innerText = `£${tipTotal}`;
+ 
