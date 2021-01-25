ea80ec03801c310fdc5e84ce3e38b9c16dd052ae
Dan Watson <dan.watson2309@gmail.com>
2021-01-25 11:22:21 +0000

    styling started

--- main.js
+++ main.js
@@ -4,68 +4,55 @@ const total = document.querySelector("#bill-total");
 const totalPeople = document.querySelector("#no-people");
 const totalTip = document.querySelector("#tip-percent");
 
+let tipTotal = 0;
 
-
-
-
-let tipTotal = 0
-
-function calcTip(){
-    let billTotal = total.value;
-    let tipPerc = totalTip.value;
-      tipTotal = (tipPerc/100) * billTotal;
-      
-};    
- 
-let grandTotal = 0
-
-function totalCost(){
+function calcTip() {
   let billTotal = total.value;
-  grandTotal= + billTotal+tipTotal;
-  
-};
+  let tipPerc = totalTip.value;
+  tipTotal = (tipPerc / 100) * billTotal;
+  tipTotall = tipTotal.toFixed(2);
+}
 
-let eachOwes = 0
+let grandTotal = 0;
 
-function totalEach(){
-     let noPeople = totalPeople.value; 
-     eachOwes = grandTotal / noPeople;
-     
-};
+function totalCost() {
+  let billTotal = total.value;
+  grandTotal = +billTotal + tipTotal;
+  grandTotall = grandTotal.toFixed(2);
+}
 
+let eachOwes = 0;
 
+function totalEach() {
+  let noPeople = totalPeople.value;
+  eachOwes = grandTotal / noPeople;
+  eachOwess = eachOwes.toFixed(2);
+}
 
-function displayTip(){
-  calcTip()
-  console.log(tipTotal)
+function displayTip() {
+  calcTip();
   let tipDisplay = document.querySelector("#tip-total");
-  tipDisplay.innerText = `£${tipTotal}`;
-    
+  tipDisplay.innerText = `£${tipTotall}`;
 }
 
-
-function displayTotal(){
-  totalCost()
-    let grandDisplay = document.querySelector("#grand-total");
-    grandDisplay.innerText = `£${grandTotal}`;
+function displayTotal() {
+  totalCost();
+  let grandDisplay = document.querySelector("#grand-total");
+  grandDisplay.innerText = `£${grandTotall}`;
 }
 
-
-function displayEachOwes(){
-  totalEach()
-    let eachDisplay = document.querySelector("#each-owes");
-    eachDisplay.innerText = `£${eachOwes}`;
+function displayEachOwes() {
+  totalEach();
+  let eachDisplay = document.querySelector("#each-owes");
+  eachDisplay.innerText = `£${eachOwess}`;
 }
 
-
-
-button.addEventListener("click", () => displayTip())
+button.addEventListener("click", () => displayTip());
 button.addEventListener("click", () => displayTotal());
 button.addEventListener("click", () => displayEachOwes());
 
+//improvements:
 
+//-add toFixed(2) to the totals
 
-
-
-
- 
+//if no.people === 0 return 1 or return grandTotal
